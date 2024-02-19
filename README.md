189dc97ac16cd709456b4b5247721a2ffed6c045
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-19 20:36:24 +0200

    init starter template using rtk + installed main dependencies

---
+++ README.md
@@ -0,0 +1,4 @@
+React  
+Redux Toolkit  
+NextUI  
+Tailwind CSS  
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Button} from "@nextui-org/react";
+
+const App = () => {
+  return (
+    <div>
+			<Button color="primary">
+				Button
+			</Button>
+    </div>
+  )
+}
+
+export default App
