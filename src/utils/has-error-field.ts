30ba6f02238de0861ab0819afdbb4c3f0b68f4cc
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-20 18:14:27 +0200

    added utils: format date && typeof backend error

---
+++ src/utils/has-error-field.ts
@@ -0,0 +1,13 @@
+export function hasErrorField(
+  err: unknown,
+): err is { data: { error: string } } {
+  return (
+    typeof err === "object" &&
+    err !== null &&
+    "data" in err &&
+		
+    typeof err.data === "object" &&
+    err.data !== null &&
+    "error" in err.data
+  )
+}
