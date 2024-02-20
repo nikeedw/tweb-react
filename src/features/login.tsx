c4d433d21884bc8b82d9009d4745c61d50a11606
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-20 19:56:22 +0200

    created custom input

---
+++ src/features/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+	setSelected: (value: string) => void;
+}
+
+const Login = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default Login
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Card, CardBody, Tab, Tabs } from '@nextui-org/react';
+import React, { useState } from 'react'
 
 const Auth = () => {
+	const [selected, setSelected] = useState('login');
+
 	return (
-		<div>
-			Auth
+		<div className='flex items-center justify-center h-screen'>
+			<div className="flex flex-col">
+				<Card className='max-w-full w-[340px] h-[450px]'>
+					<CardBody className='overflow-hidden'>
+						<Tabs
+							fullWidth
+							size='md'
+							selectedKey={selected}
+							onSelectionChange={(key) => setSelected(key as string)}
+						>
+							<Tab key='login' title='Вход'>
+								Вход
+							</Tab>
+							<Tab key='sign up' title='Регистрация'>
+								Регистрация
+							</Tab>
+						</Tabs>
+					</CardBody>
+				</Card>
+			</div>
 		</div>
 	)
 }
