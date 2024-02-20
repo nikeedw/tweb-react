c0ede6fc72ce63bf19a5fac3e1cb596497a4dd5c
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-20 11:02:54 +0200

    likes api

---
+++ src/app/services/likesApi.ts
@@ -0,0 +1,29 @@
+import { Like } from "../types"
+import { api } from "./api"
+
+export const likesApi = api.injectEndpoints({
+	endpoints: (builder) => ({
+		likePost: builder.mutation<Like, { postId: string }>({
+			query: (body) => ({
+				url: `/likes`,
+				method: "POST",
+				body
+			}),
+		}),
+		unlikePost: builder.mutation<void, string>({
+			query: (postId) => ({
+				url: `/likes/${postId}`,
+				method: "DELETE",
+			})
+		})
+	})
+})
+
+export const {
+	useUnlikePostMutation,
+	useLikePostMutation
+} = likesApi
+
+export const {
+	endpoints: { likePost, unlikePost }
+} = likesApi
