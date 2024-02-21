610eccadda5de5f9e794381064c17de7f3bc273f
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-21 20:04:48 +0200

    fixed auth path error

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../../constants";
-import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: `${BASE_URL}/api`,
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,26 +1,27 @@
 import { Card, CardBody, Tab, Tabs } from '@nextui-org/react';
 import React, { useState } from 'react'
-import Login from '../../features/login';
+import { Login } from '../../features/login';
+import { Register } from '../../features/register';
 
 const Auth = () => {
 	const [selected, setSelected] = useState('login');
 
 	return (
-		<div className='flex items-center justify-center h-screen'>
+		<div className="flex items-center justify-center h-screen">
 			<div className="flex flex-col">
-				<Card className='max-w-full w-[340px] h-[450px]'>
-					<CardBody className='overflow-hidden'>
+				<Card className="max-w-full w-[340px] h-[450px]">
+					<CardBody className="overflow-hidden">
 						<Tabs
 							fullWidth
-							size='md'
+							size="md"
 							selectedKey={selected}
 							onSelectionChange={(key) => setSelected(key as string)}
 						>
-							<Tab key='login' title='Вход'>
-								<Login setSelected={setSelected}/>
+							<Tab key="login" title="Вход">
+								<Login setSelected={setSelected} />
 							</Tab>
-							<Tab key='sign up' title='Регистрация'>
-								Регистрация
+							<Tab key="sign-up" title="Регистрация">
+								<Register setSelected={setSelected} />
 							</Tab>
 						</Tabs>
 					</CardBody>
