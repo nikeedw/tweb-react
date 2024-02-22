9c9ddf8563eaacbc20f396a6ef4f071fb6387206
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-22 19:08:50 +0200

    added create-post component

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useGetAllPostsQuery } from '../../app/services/postsApi'
+import { CreatePost } from '../../components/create-post';
 
 const Posts = () => {
+	const { data } = useGetAllPostsQuery();
+
 	return (
-		<div>
-			Posts
-		</div>
+		<>
+			<div className="mb-10 w-full">
+				<CreatePost />
+			</div>
+		</>
 	)
 }
 
